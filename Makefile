b43a537da655a3c38bdf06caefae120d7f6aa0ac
student-404-error <127807924+student-404-error@users.noreply.github.com>
2024-06-25 08:09:10 +0200

    초기 세팅

---
+++ Makefile
@@ -0,0 +1,66 @@
+# ANSI COLOR CODE
+RED		:=	\033[31m
+GREEN		:=	\033[32m
+YELLOW		:=	\033[33m
+BLUE		:=	\033[34m
+MAGENTA		:=	\033[35m
+CYAN		:=	\033[36m
+WHITE		:=	\033[37m
+RESET		:=	\033[0m
+
+NAME		=	fdf
+
+INCLUDES	=	./include
+
+SRC_DIR		=	./src/fdf
+SRCS		=	$(wildcard $(SRC_DIR)/*.c)
+
+LIBFTNAME	=	libft.a
+LIBFT_PATH	=	./src/libft
+
+MINILIBX	=	/usr/local/lib
+MINILIBX_FLAG	=	-L$(MINILIBX) -lmlx -framework OpenGL -framework AppKit
+CC		=	gcc
+CFLAGS		=	-Wall -Werror -Wextra -g -I$(INCLUDES)
+
+OBJS		=	$(SRCS:.c=.o)
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
+#======================RULES=======================#
+
+all		:	$(NAME)
+
+$(NAME)		:	$(OBJS)
+			@echo "$(GREEN)==========================$(RESET)"
+			@echo "$(GREEN) Building $(MAGENTA)fdf$(GREEN)...$(RESET)"
+			@make -C $(LIBFT_PATH)
+			@$(CC) $(CFLAGS) $(MINILIBX_FLAG) $(OBJS) $(LIBFT_PATH)/$(LIBFTNAME) -o $(NAME)
+			@echo "$(GREEN) Build complete!$(RESET)"
+			@echo "$(GREEN)==========================$(RESET)"
+
+clean		:
+			@echo "$(RED)===========$(WHITE)clean$(RED)===========$(RESET)"
+			@echo "$(RED)Cleaning up...$(RESET)"
+			@rm -f $(OBJS)
+			@cd $(LIBFT_PATH) && make clean
+			@echo "$(RED)Cleaning complete!$(RESET)"
+			@echo "$(RED)===========$(WHITE)clean$(RED)===========$(RESET)"
+
+fclean		:	clean
+			@echo "$(RED)===========$(CYAN)fclean$(RED)===========$(RESET)"
+			@echo "$(RED)Deleting .a files...$(RESET)"
+			@rm -f $(NAME)
+			@cd $(LIBFT_PATH) && make fclean
+			@echo "$(RED)Delete complete!$(RESET)"
+			@echo "$(RED)===========$(CYAN)fclean$(RED)===========$(RESET)"
+
+re		:	fclean	\
+			all
+
+.PHONY		:	all	\
+			clean	\
+			fclean	\
+			bonus	\
+			re
---
+++ include/push_swap.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:19:25 by seong-ki          #+#    #+#             */
+/*   Updated: 2024/06/21 20:37:57 by seong-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "libft.h"
+# define ABS(Value) (Value < 0 ? -Value : Value)
+# define MAX(first, second) (first > second ? first : second)
+# define MAX_FLAG(first, second) (first > second ? 1 : 0)
+# define MIN(first, second) (first < second ? first : second)
+# define MIN_FLAG(first, second) (first < second ? 1 : 0)
+
+typedef struct s_stack
+{
+	t_list	*top;
+	t_list	*bottom;
+	int		max;
+	int		min;
+}	t_stack;
+void	find_index(t_list *a);
+int		find_lis(t_list *a_list, int size);
+//void	push_swap(t_list **a_stack, t_list **b_stack);
+int		check_sort(t_list *a);
+void	passing_to_b(t_list **a, t_list **b);
+void	sort(t_list **a, t_list **b, t_stack **a_stk, t_stack **b_stk);
+void	move_swap(t_list **lst, t_stack **stk, char *title);
+void	move_ss(t_list **a_stack, t_list **b_stack);
+void	move_push(t_list **to_lst, t_list **fr_lst, t_stack **to, t_stack **fr);
+void	move_rotate(t_list **lst, t_stack **stack, char *title);
+void	move_rr(t_list **a, t_list **b, t_stack **a_stk, t_stack **b_stk);
+void	move_reverse_rotate(t_list **lst, t_stack **stack, char *title);
+void	move_rrr(t_list **a, t_list **b, t_stack **a_stk, t_stack **b_stk);
+void	status_list_and_stack(t_list *lst, t_stack *stk, char *title);
+void	three_argu(t_list **a_list, t_stack **a_stk);
+void	last_sort(t_list **a, t_stack **a_stk);
+t_stack	*ft_new_stack(t_list *lst);
+
+#endif
