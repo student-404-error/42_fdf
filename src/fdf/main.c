df6adb91dc21b8c0bd8559ee55ae4c455983470c
student-404-error <ksh30102@gmail.com>
2024-06-28 16:06:17 +0200

    gnl문제 해결

--- src/fdf/main.c
+++ src/fdf/main.c
@@ -1,17 +1,31 @@
-#include <mlx.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "get_next_line.h"
 #include <stdio.h>
+#include <libft.h>
 
 int main(int argc, char *argv[])
 {
 	int	fd;
-	if (argc < 2)
+	(void) argc;
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Failed to read\n");
 		return (1);
-	(void) argv;
-	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
+	}
+	char	*line;
+	line = get_next_line(fd);
+	printf("line: %s", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("line: %s", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("line: %s", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("line: %s", line);
+	free(line);
 	return EXIT_SUCCESS;
 }
