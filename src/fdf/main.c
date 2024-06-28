fbdda3278bc48c0a465e6a1335af0bb6671c47c3
student-404-error <127807924+student-404-error@users.noreply.github.com>
2024-06-28 15:12:21 +0200

    update

--- src/fdf/main.c
+++ src/fdf/main.c
@@ -3,13 +3,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "get_next_line.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	int	fd;
 	if (argc < 2)
 		return (1);
-	fd = open(argv[1], O_RDONLY);
+	(void) argv;
+	fd = open("test.txt", O_RDONLY);
 	printf("%s", get_next_line(fd));
 	return EXIT_SUCCESS;
 }
