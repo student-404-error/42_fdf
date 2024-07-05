de52115704d73fdcef19adcfb358b2621de7721c
student-404-error <ksh30102@gmail.com>
2024-07-05 13:56:17 +0200

    Update fdf.h

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: seong-ki <seong-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 15:27:29 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/06/30 15:28:47 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/07/05 13:55:56 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,22 @@
 # include <fcntl.h>
 
 # define PI 3.1415926
-typedef struct	s_data {
+# define WIDTH 500
+# define HEIGHT 500
+
+typedef struct s_point {
+	float	x;
+	float	y;
+	int		flag;
+}				t_point;
+
+typedef struct s_map {
+	struct s_point	**matrix;
+	int				column;
+	int				row;
+}				t_map;
+
+typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -28,7 +43,7 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-typedef struct	s_vars {
+typedef struct s_vars {
 	void	*win;
 	void	*mlx;
 }				t_vars;
@@ -36,4 +51,3 @@ typedef struct	s_vars {
 int		open_file(int ac, char **av);
 void	parse_map(int fd);
 #endif // !FDF_H
-
