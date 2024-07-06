d2ee5c7fafbda1b8f8c263830577625a7224c1c0
student-404-error <ksh30102@gmail.com>
2024-07-06 19:30:30 +0200

    bfs test

--- src/fdf/parse_map.c
+++ src/fdf/parse_map.c
@@ -6,30 +6,44 @@
 /*   By: seong-ki <seong-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 17:15:46 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/07/05 14:48:45 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/07/06 18:03:52 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "fdf.h"
 
 // void	create_matrix(char *line, int row, int column)
 // {
 //
 // }
 
-void	create_row(char *line)
+t_point	*create_point(int x, int y, int z)
+{
+	t_point	*point;
+
+	point = malloc(sizeof(t_point));
+	point->x = cos(30 * PI / 180) * (x - y);
+	point->y = sin(30 * PI / 180) * (x + y) - z;
+	point->flag = 0;
+	return (point);
+}
+
+void	create_row(int line_num, char *line)
 {
 	char	**row;
 	int		i;
+	t_point	*p;
 
-	row = ft_split(line, ' ');	// row is allocated. It must be freed
+	row = ft_split(line, ' ');	// row is allocated. It must be freed.
 	i = 0;
 	while (row[i] && row[i][0] != '\n')
 	{
-		ft_printf("%d\n", ft_atoi(row[i]));
+		p = create_point(i, line_num, ft_atoi(row[i]));
+	//	printf("%f %f %d\n", p->x, p->y, p->flag);
+		free(p);
 		i++;
 	}
-	ft_free_split(row);
+	ft_free_split(row);			// row is freed here.
 }
 
 void	parse_map(int fd)
@@ -43,7 +57,7 @@ void	parse_map(int fd)
 	while (line)
 	{
 		ft_printf("%s", line);
-		create_row(line);
+		create_row(row, line);
 		free(line);
 		line = get_next_line(fd);
 	//	if (column != (int)ft_strlen(line) && ft_strlen(line) != 0)
