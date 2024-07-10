bbca3432c93492f79bfc6bbd2e59f4cf8b7098a6
student-404-error <ksh30102@gmail.com>
2024-07-11 00:17:36 +0200

    freed error

--- src/fdf/parse_map.c
+++ src/fdf/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: seong-ki <seong-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 17:15:46 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/07/07 16:06:48 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/07/10 22:53:33 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -30,11 +30,14 @@ float	*get_color(char *line)
 	// color will be 0x-------- or nothing
 }
 */
+
 t_point	*create_point(int x, int y, int z/*, char *line*/)
 {
 	t_point	*point;
 
 	point = (t_point *) malloc(sizeof(t_point));
+	if (!point)
+		return (NULL);
 	point->x = cos(30 * PI / 180) * (x - y);
 	point->y = sin(30 * PI / 180) * (x + y) - z;
 	//point->color = get_color(line);
@@ -57,8 +60,21 @@ void	join_point(t_point **row, t_point *new_point)
 		last_node = *row;
 		while (last_node->next)
 			last_node = last_node->next;
-		printf("%f %f\n", last_node->x, last_node->y);
 		last_node->next = new_point;
+	}
+}
+
+void	ft_free_points(t_point *pt)
+{
+    t_point *free_point;
+    t_point *tmp_point;
+
+	free_point = pt;
+	while (free_point)
+	{
+		tmp_point = free_point->next;
+		free(free_point);
+		free_point = tmp_point;
 	}
 }
 
@@ -66,9 +82,9 @@ t_line	*create_row(int y, char *line)
 {
 	int			x;
 	char		**points;
+	t_line		*row;
 	t_point		*pt;
 	static int	column;
-	t_line		*row;
 
 	row = malloc(sizeof(t_line));
 	if (!row)
@@ -81,21 +97,26 @@ t_line	*create_row(int y, char *line)
 		join_point(&pt, create_point(x, y, ft_atoi(points[x])));
 		x++;
 	}
+	ft_free_split(points);			// row is freed here.
 	if (column != 0 && column != x)
 	{
-		printf("error\n");
-		exit(1);
+		printf("Find wrong length!!\n");
+		free(row);
+		ft_free_points(pt);
+		return (NULL);
 	}
 	column = x;
-	ft_free_split(points);			// row is freed here.
 	row->line = pt;
 	row->next = NULL;
 	return (row);
 }
-void	join_line(t_line **matrix, t_line *new_row)
+
+int	join_line(t_line **matrix, t_line *new_row)
 {
 	t_line	*last_row;
 
+	if (new_row == NULL)
+		return (-1);
 	if (*matrix == NULL)
 		*matrix = new_row;
 	else
@@ -105,26 +126,69 @@ void	join_line(t_line **matrix, t_line *new_row)
 			last_row = last_row->next;
 		last_row->next = new_row;
 	}
+	return (0);
 }
+
 void	prt_matrix(t_map *map)
 {
 	int	i;
 	int	j;
+	t_line	*row;
+	t_point	*point;
 	
+	row = map->matrix;
 	i = 0;
-	while (map->matrix)
+	while (row)
 	{
 		j = 0;
-		while (map->matrix->line)
+		point = row->line;
+		while (point)
 		{
-			printf("%d %d: %f %f |", i, j, map->matrix->line->x, map->matrix->line->y);
-			map->matrix->line = map->matrix->line->next;
+			printf("%d %d: %f %f\n", i, j, point->x, point->y);
+			point = point->next;
 			j++;
 		}
-		printf("\n");
-		map->matrix = map->matrix->next;
+		printf("===================\n");
+		row = row->next;
 		i++;
 	}
+}
+
+void	ft_free_map(t_map *map)
+{
+	t_line	*free_row;
+	t_line	*tmp_row;
+	t_point	*free_point;
+	t_point	*tmp_point;
+
+	free_row = map->matrix;
+	while (free_row)
+	{
+		tmp_row = free_row->next;
+		free_point = free_row->line;
+		while (free_point)
+		{
+			tmp_point = free_point->next;
+			free(free_point);
+			free_point = tmp_point;
+		}
+		free(free_row);
+		free_row = tmp_row;
+	}
+	free(map);
+}
+
+t_map	*init_map(void)
+{
+	t_map	*map;
+
+	map = malloc(sizeof(t_map));
+	if (map == NULL)
+		return (NULL);
+	map->matrix = NULL;
+	map->column = 0;
+	map->row = 0;
+	return (map);
 }
 
 t_map	*parse_map(int fd)
@@ -134,21 +198,22 @@ t_map	*parse_map(int fd)
 	t_map	*map;
 	//	int	column;
 
-	map = malloc(sizeof(t_map));
-	if (map == NULL)
-		return (NULL);
+	map = init_map();
 	y = 0;
-	line = get_next_line(fd);
-	while (line)
+	while ((line = get_next_line(fd, 1)) != NULL)
 	{
 		ft_printf("%s", line);
-		join_line(&map->matrix, create_row(y, line));	// 한 줄이 반환이 되는거임.
+		if (join_line(&map->matrix, create_row(y, line)) == -1)	// 한 줄이 반환이 되는거임.
+		{
+			ft_free_map(map);
+			free(line);
+			get_next_line(fd, -1);
+			return (NULL);
+		}
 		free(line);
-		line = get_next_line(fd);
 		y++;
 	}
 	prt_matrix(map);
-	// create_points();
-	// isometric_projection();
-	return (map);
+	ft_free_map(map);
+	return (NULL);
 }
